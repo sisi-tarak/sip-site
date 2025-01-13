5750b0fd7bd2274da2f9c85cee344b4a7d5646b0
sisi-tarak <sisindrisingamsetti@gmail.com>
2025-01-13 18:12:37 +0530

    colors and scroll bar updated

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,9 +1,12 @@
+import { Container } from "@mui/material";
 import React from "react";
 
 const Homepage = () => {
   return (
     <>
-      <h1 className="text-3xl text-headColor">Homepage</h1>
+      <Container maxWidth="lg" className="h-screen">
+        <h1 className="text-3xl text-headColor">Homepage</h1>
+      </Container>
     </>
   );
 };
